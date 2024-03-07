d493c5850401ff03d4b3de1572425b463f4c88fb
Prashant Tiwari <shivtiwari5021@gmail.com>
2024-03-07 17:35:12 +0000

    used object in array

--- 01_Arrays/01_Array_traversing.js
+++ 01_Arrays/01_Array_traversing.js
@@ -1,10 +1,27 @@
 let values=[9,45,7,1,2,8,0,11,23,41,77];
-for (let index = 0; index < values.length; index++) {
-    const element = values[index];                      //Traditional for loop
-    console.log(`${index}: value is ${element}`);       //time complexity is: O(n)
+// for (let index = 0; index < values.length; index++) {
+//     const element = values[index];                      //Traditional for loop
+//     console.log(`${index}: value is ${element}`);       //time complexity is: O(1)
+// }
+
+
+// values.forEach(element => {                 //using forEach it has callback and TC is quadratic
+//     console.log(element);                  //time complexity is: O(n) linear 
+// });
+
+const result=Array.from(values)
+console.log(result);
+
+//passing object in array
+let Person={
+    name:"Someone",
+    age:26,
+    location:"India"
 }
 
+let objectPassedArray=[1,2,3,4,5,Person]
+console.log(objectPassedArray);
 
-values.forEach(element => {                 //using forEach it has callback and TC is quadratic
-    console.log(element);                  //time complexity is: O(n)
-});
+objectPassedArray.forEach((ele)=>{    //Not the best approach
+    console.log(ele)                
+})
---
+++ 01_Arrays/02_Array_add_remove_Element.js
@@ -0,0 +1,10 @@
+//Add and Remove Element
+
+//Add or append
+let arr0=[1,2,3,4,5,6];                   //insertion in array
+arr0.push("Hello");                      //time complexity of push is: O(1) 
+console.log(arr0);
+
+let arr1=["Apple","Mango","Banana",2,"Batman"]
+arr1.pop()                               //pop() doesn't require value because it delete a value from the end
+console.log(arr1);                       //time complexity of pop is: O(1)                            
